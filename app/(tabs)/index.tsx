de274cd4bb8ac89564aaed065a907bc59730bda1
steven kamange <kamangasteven00@gmail.com>
2024-06-03 08:42:41 +0200

    added theme  provider

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,30 @@
-import "../../global.css";
 import { StatusBar } from "expo-status-bar";
 import { Button, Text, View } from "react-native";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { useEffect, useState } from "react";
 import { Box } from "@/components/ui/box";
 import { router } from "expo-router";
 import { Center } from "@/components/ui/center";
 
 export default function Page() {
-  const [uiState, setUiState] = useState<"light" | "dark">("light");
-
-  useEffect(() => {
-    setUiState("dark");
-    // Get Ui State from AsyncStorage
-  }, []);
   return (
-    <GluestackUIProvider mode={uiState}>
-      <Center className="flex-1 bg-primary-500">
-        <Box>
-          <Text className="text-typography-100">
-            Open up App.tsx to start working on your app!
-          </Text>
-          <Text className="text-typography-100"></Text>
-        </Box>
-        <Button
-          onPress={() => {
-            router.push({
-              pathname: "/event-details/[id]",
-              params: {
-                id: "123",
-              },
-            });
-          }}
-          title={uiState === "light" ? "Dark" : "Light"}
-        />
-        <StatusBar style="auto" />
-      </Center>
-    </GluestackUIProvider>
+    <Center className="flex-1 bg-primary-800">
+      <Box>
+        <Text className="text-typography-100">
+          Open up App.tsx to start working on your app!
+        </Text>
+        <Text className="text-typography-100"></Text>
+      </Box>
+      <Button
+        onPress={() => {
+          router.push({
+            pathname: "/event-details/[id]",
+            params: {
+              id: "123",
+            },
+          });
+        }}
+        title={"Button"}
+      />
+      <StatusBar style="auto" />
+    </Center>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,38 @@
+import "../global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
 
 const RootLayout = () => {
+  const [uiState, setUiState] = useState<"light" | "dark">("light");
+
+  useEffect(() => {
+    setUiState("light");
+    // Get Ui State from AsyncStorage
+  }, []);
+  const colorScheme = useColorScheme();
   return (
-    <Stack>
-      <Stack.Screen
-        name="(tabs)"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen name="event-details/[id]" />
-    </Stack>
+    <GluestackUIProvider mode={uiState}>
+      <ThemeProvider
+        value={colorScheme === `${uiState}` ? DarkTheme : DefaultTheme}
+      >
+        <Stack>
+          <Stack.Screen
+            name="(tabs)"
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen name="event-details/[id]" />
+        </Stack>
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 };
 
